7176647499645b5d62fd072471c0ab7f3d49db8c
sebacallejas <sebastian.callejas@inacapmail.cl>
2021-04-27 20:27:17 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Clase02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cadena = "Salí a comprar, aun estando en cuarentena";
+            string[] array = cadena.Split('a');
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
